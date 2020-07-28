bde3914b13da282ceade92179bb1d36562a79e5b
Colin Bate <colin@colinbate.com>
2020-07-28 16:33:16 -0300

    Add example of a custom style

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,15 @@
 	import Tailwindcss from './Tailwindcss.svelte';
 	export let name: string;
 </script>
+<style>
+	.custom-style {
+		@apply italic;
+	}
+</style>
 <Tailwindcss />
 <main class="p-4 mx-auto text-center max-w-xl">
 	<h1 class="uppercase text-6xl font-hairline text-svelte">Hello {name}!</h1>
-	<p>
+	<p class="custom-style">
 		Visit the
 		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
 		to learn how to build Svelte apps.
