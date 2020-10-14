a01951537b0590057689dedd285b1c7b8631bd8c
Colin Bate <colin@colinbate.com>
2020-10-14 16:44:57 -0300

    Update to Tailwind 1.9 - Future features enabled.

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,7 @@
 <Tailwindcss />
 <ModeSwitcher />
 <main class="p-4 mx-auto text-center max-w-xl">
-	<h1 class="uppercase text-6xl font-hairline text-svelte">Hello {name}!</h1>
+	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">Hello {name}!</h1>
 	<p class="custom-style">
 		Visit the
 		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
